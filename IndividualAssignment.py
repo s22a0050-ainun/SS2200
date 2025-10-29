d186db40b69db0c5033f43ceea411332cf60ffbe
s22a0050-ainun <s22a0050@siswa.umk.edu.my>
2025-10-29 19:58:35 +0800

    Update IndividualAssignment.py

--- IndividualAssignment.py
+++ IndividualAssignment.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as px
 
 st.set_page_config(
     page_title="Scientific Visualization"
@@ -6,18 +8,10 @@ st.set_page_config(
 
 st.header("Scientific Visualization", divider="gray")
 
-import streamlit as st
-import pandas as pd
-import plotly.express as px
-
 st.set_page_config(page_title="GitHub Data Loader", layout="wide")
 st.title("Student Mental Health")
 
 url = 'https://raw.githubusercontent.com/s22a0050-ainun/SS2200/refs/heads/main/Student_Mental_Health.csv'
-
-import streamlit as st
-import pandas as pd
-import plotly.express as px
 
 # Streamlit page setup
 st.set_page_config(page_title="Student Mental Health Dashboard")
