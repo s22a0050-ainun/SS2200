ff53d366a40478041a57916567a557c4e1f5e4f0
s22a0050-ainun <s22a0050@siswa.umk.edu.my>
2025-10-29 19:26:23 +0800

    Update IndividualAssignment.py

--- IndividualAssignment.py
+++ IndividualAssignment.py
@@ -1,16 +1,16 @@
 import streamlit as st
+
+st.set_page_config(
+    page_title="Scientific Visualization"
+)
+
+st.header("Scientific Visualization", divider="gray")
+
+import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.set_page_config(page_title="Student Mental Health Dashboard", layout="wide"
-                   
-st.title("🎓 Student Mental Health Dashboard")
-st.write("Explore and visualize data about student mental health.")
+st.set_page_config(page_title="GitHub Data Loader", layout="wide")
+st.title("Student Mental Health")
 
-uploaded_file = st.file_uploader("https://raw.githubusercontent.com/s22a0050-ainun/SS2200/refs/heads/main/Student_Mental_Health.csv", type=["csv"])
-
-if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)
-
-    st.subheader("📊 Dataset Preview")
-    st.dataframe(df.head())
+url = ''
