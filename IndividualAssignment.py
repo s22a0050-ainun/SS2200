e19a0797fd8faa7f33dd8500d80b9ac9a7c7c91f
s22a0050-ainun <s22a0050@siswa.umk.edu.my>
2025-10-29 18:41:13 +0800

    Update IndividualAssignment.py

--- IndividualAssignment.py
+++ IndividualAssignment.py
@@ -1,15 +1,17 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Scientific Visualization"
-)
-
-st.header("Scientific Visualization", divider="gray")
-
-st.set_page_config(
     page_title="Individual Assignment"
 )
 
 visualise = st.Page('IndividualAssignment.py', title='Student Mental Health', icon=":material/school:")
+
+home = st.Page('home.py', title='Homepage', default=True, icon=":material/home:")
+
+pg = st.navigation(
+        {
+            "Menu": [home, visualise]
+        }
+    )
 
 pg.run()
