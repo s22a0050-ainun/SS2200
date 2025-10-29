6b3c3bdc2e50fef7d65a7dcf1e10af184d1d849f
s22a0050-ainun <s22a0050@siswa.umk.edu.my>
2025-10-29 19:50:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,35 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Tutorial 3"
+    page_title="Student Dashboard",
+    page_icon=":material/menu_book:",
+    layout="wide"
 )
 
-visualise = st.Page('tutorial3.py', title='Pencapaian Akademik Pelajar', title='Student Mental Health' icon=":material/school:")
+home = st.Page(
+    'home.py',
+    title='🏠 Homepage',
+    default=True,
+    icon=":material/home:"
+)
 
-home = st.Page('home.py', title='Homepage', default=True, icon=":material/home:")
+tutorial3 = st.Page(
+    'tutorial3.py',
+    title='📘 Tutorial 3: Pencapaian Akademik Pelajar',
+    icon=":material/school:"
+)
+
+mental_health = st.Page(
+    'student_mental_health.py',
+    title='🧠 Student Mental Health',
+    icon=":material/psychology:"
+)
 
 pg = st.navigation(
-        {
-            "Menu": [home, visualise]
-        }
-    )
+    {
+        "Menu": [home, tutorial3, mental_health]
+    }
+)
 
 pg.run()
+
