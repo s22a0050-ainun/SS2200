9afac6eb5f873c35a06f91e7cba94a862d818d9b
s22a0050-ainun <s22a0050@siswa.umk.edu.my>
2025-10-29 20:04:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -16,13 +16,20 @@ pg = st.navigation(
 
 pg.run()
 
-import streamlit as st
-
 st.set_page_config(
     page_title="Student Mental Health"
 )
 
 visualise = st.Page('IndividualAssignment.py', title='Student Mental Health', icon=":material/school:")
+
+
+home = st.Page('home.py', title='Homepage', default=True, icon=":material/home:")
+
+pg = st.navigation(
+        {
+            "Menu": [home, visualise]
+        }
+    )
 
 
 pg.run()
