909a97e9de44b596e31e6511547fb58be05a4a2e
s22a0050-ainun <s22a0050@siswa.umk.edu.my>
2025-10-29 18:44:00 +0800

    Update IndividualAssignment.py

--- IndividualAssignment.py
+++ IndividualAssignment.py
@@ -13,4 +13,4 @@ import plotly.express as px
 st.set_page_config(page_title="GitHub Data Loader", layout="wide")
 st.title("Student Mental Health")
 
-url = 'https://raw.githubusercontent.com/s22a0050-ainun/SS2200/refs/heads/main/arts_faculty_data.csv'
+url = 'https://raw.githubusercontent.com/s22a0050-ainun/SS2200/refs/heads/main/Student_Mental_Health.csv'
