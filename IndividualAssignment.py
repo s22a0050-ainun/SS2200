f82a05311748db90428c51f233dbcdc34c5284ba
s22a0050-ainun <s22a0050@siswa.umk.edu.my>
2025-10-29 19:21:09 +0800

    Update IndividualAssignment.py

--- IndividualAssignment.py
+++ IndividualAssignment.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="Student Mental Health Dashboard", layout="wide"
 st.title("🎓 Student Mental Health Dashboard")
 st.write("Explore and visualize data about student mental health.")
 
-uploaded_file = st.file_uploader("Upload your CSV file", type=["csv"])
+uploaded_file = st.file_uploader("https://raw.githubusercontent.com/s22a0050-ainun/SS2200/refs/heads/main/Student_Mental_Health.csv", type=["csv"])
 
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
