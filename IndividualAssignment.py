ff4b8700cb8b44335d4d567e392ad9e66d8cc6b1
s22a0050-ainun <s22a0050@siswa.umk.edu.my>
2025-10-29 18:33:15 +0800

    Update IndividualAssignment.py

--- IndividualAssignment.py
+++ IndividualAssignment.py
@@ -5,3 +5,11 @@ st.set_page_config(
 )
 
 st.header("Scientific Visualization", divider="gray")
+
+st.set_page_config(
+    page_title="Individual Assignment"
+)
+
+visualise = st.Page('IndividualAssignment.py', title='Student Mental Health', icon=":material/school:")
+
+pg.run()
