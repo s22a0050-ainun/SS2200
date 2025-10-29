3422b3015dc02af262566e1dd5deaf42a47a4f06
s22a0050-ainun <s22a0050@siswa.umk.edu.my>
2025-10-29 20:16:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Tutorial 3",
+    page_title1="Tutorial 3",
 
-    page_title="Student Mental Health Dashboard"
+    page_title2="Student Mental Health"
 )
 
 visualise = st.Page('tutorial3.py', title='Pencapaian Akademik Pelajar', icon=":material/school:"),
