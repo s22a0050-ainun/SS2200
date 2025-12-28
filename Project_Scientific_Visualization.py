041a91756bdbbc127d1785103583cb89109358ff
s22a0050-ainun <s22a0050@siswa.umk.edu.my>
2025-12-28 19:05:51 +0800

    Update Project_Scientific_Visualization.py

--- Project_Scientific_Visualization.py
+++ Project_Scientific_Visualization.py
@@ -14,12 +14,12 @@ st.write("""
 """)
 
 st.set_page_config(page_title="GitHub Data Loader", layout="wide")
-st.title("Exploring Internet Use and Suicidality")
+st.title("Exploring Internet Use and Suicidality in Mental Health Populations")
 
 url = 'https://raw.githubusercontent.com/s22a0050-ainun/SS2200/refs/heads/main/Exploring%20Internet%20Use%20and%20Suicidality%20in%20Mental%20Health%20Populations.csv'
 
 # Streamlit page setup
-st.set_page_config(page_title="Exploring Internet Use and Suicidality Dashboard")
+st.set_page_config(page_title="Exploring Internet Use and Suicidality in Mental Health Populations Dashboard")
 
 # Load data from GitHub
 url = 'https://raw.githubusercontent.com/s22a0050-ainun/SS2200/refs/heads/main/Exploring%20Internet%20Use%20and%20Suicidality%20in%20Mental%20Health%20Populations.csv'
@@ -27,7 +27,7 @@ url = 'https://raw.githubusercontent.com/s22a0050-ainun/SS2200/refs/heads/main/E
 try:
     df = pd.read_csv(url)
     st.success("✅ Data loaded successfully from GitHub!")
-    st.write("Dataset of Exploring Internet Use and Suicidality")
+    st.write("Dataset of Exploring Internet Use and Suicidality in Mental Health Populations")
     st.dataframe(df.head())
 except Exception as e:
     st.error(f"An error occurred while loading data: {e}")
