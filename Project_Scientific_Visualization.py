3400c7f336f60a88ce6fc625dc8f613b3265980a
s22a0050-ainun <s22a0050@siswa.umk.edu.my>
2025-12-31 16:40:06 +0800

    Update Project_Scientific_Visualization.py

--- Project_Scientific_Visualization.py
+++ Project_Scientific_Visualization.py
@@ -10,21 +10,21 @@ st.header("Scientific Visualization : Project Group", divider="gray")
 
 st.subheader("🎯 Objective Statement")
 st.write("""
-The purpose of this visualization is to analyze the relationship between student's gender, 
-academic courses and mental health conditions such as depression, anxiety and panic attacks. 
-This can help in understanding how different factors contribute to mental well-being among students.
+The purpose of this visualization is to identify and analyze the demographic differences in mental health experiences 
+among students, with a particular focus on how factors such as gender, age, race and year of study influence student's 
+perceptions and experiences of mental health challenges."
 """)
 
 st.set_page_config(page_title="GitHub Data Loader", layout="wide")
 st.title("Exploring Internet Use and Suicidality in Mental Health Populations")
 
-url = 'https://raw.githubusercontent.com/s22a0050-ainun/SS2200/refs/heads/main/Student_Mental_Health.csv'
+url = 'https://raw.githubusercontent.com/s22a0050-ainun/SS2200/refs/heads/main/Exploring%20Internet%20Use%20and%20Suicidality%20in%20Mental%20Health%20Populations.csv'
 
 # Streamlit page setup
 st.set_page_config(page_title="Exploring Internet Use and Suicidality in Mental Health Populations Dashboard")
 
 # Load data from GitHub
-url = 'https://raw.githubusercontent.com/s22a0050-ainun/SS2200/main/Student_Mental_Health.csv'
+url = 'https://raw.githubusercontent.com/s22a0050-ainun/SS2200/refs/heads/main/Exploring%20Internet%20Use%20and%20Suicidality%20in%20Mental%20Health%20Populations.csv'
 
 try:
     df = pd.read_csv(url)
